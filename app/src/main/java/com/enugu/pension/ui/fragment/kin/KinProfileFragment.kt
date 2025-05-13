b3cf1b2cef687685168ba4577b38dbf35266d812
Muhammad Faisal <muhammad.faisal@techversantinfotech@gmail.com>
2025-05-13 16:52:54 +0530

    feat: Account details & Next of Kin

--- app/src/main/java/com/enugu/pension/ui/fragment/kin/KinProfileFragment.kt
+++ app/src/main/java/com/enugu/pension/ui/fragment/kin/KinProfileFragment.kt
@@ -37,8 +37,7 @@ class KinProfileFragment : BaseFragment() {
         initViewModels()
         initViews()
         observeLiveData()
-        showLoader()
-        viewModel.fetchProfileDetails()
+        loadDetails()
     }
 
     private fun initViewModels() {
@@ -63,6 +62,13 @@ class KinProfileFragment : BaseFragment() {
             if (response?.detail?.status == AppConstants.SUCCESS) {
                 populateViews()
             }
+        }
+    }
+
+    private fun loadDetails() {
+        if (confirmInternet()) {
+            showLoader()
+            viewModel.fetchProfileDetails()
         }
     }
 
